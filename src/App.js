c6096ccfe9b8abc9049ce96e388747999dde030e
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2022-12-19 12:42:46 +0530

    All bugs Fixed

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import List from "./components/List";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/List' element={<List />} />
-      <Route path="/" element={<Input />} />
+      <Routes>
+        <Route path="/" element={<Input />} />
       </Routes>
     </BrowserRouter>
   );
